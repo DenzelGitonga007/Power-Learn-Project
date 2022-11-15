751387e379c70c015de37da54927f4a1230b0cba
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-15 19:55:43 +0300

    Dart

---
+++ Dart/introduction.dart
@@ -0,0 +1,15 @@
+// Data types
+void main() {
+  // Maps -- these are dictionaries in Py
+  Map me = {
+    'name' : 'Denzel Gitonga',
+    'age' : '20',
+    'networth' : '100000000'
+  };
+  // Strings
+  String f_name = "Denzel";
+  String l_name = "Gitonga";
+  print(me); //The Map
+  print(f_name + " " + l_name); // Concatenation
+  print("$f_name $l_name 'Dollar sign'");
+}
