6e9d31f7475269e366dc74e0a0e7f154bc81d99e
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-17 14:08:52 +0300

    guessing game

---
+++ Python_PLP/Tasks/random_guess.py
@@ -0,0 +1,15 @@
+# Write a Python program to guess a number between 1 to 9.
+
+# Note : User is prompted to enter a guess. If the user guesses
+#  wrong then the prompt appears again until the guess is correct, 
+#  on successful guess, user will get a "Well guessed!" message, and the program will exit.
+import random
+magic_number = random.randint(1, 9) # The number guessed randint
+print("The magic number is: ", magic_number)
+# Get the user input
+user_guess = int(input("Guess the magic number... enter it below: "))
+# Check whether the numbers match
+while user_guess != magic_number:
+    user_guess = int(input("Try another guess... enter it below: "))
+# else:
+print("Well guessed!")
