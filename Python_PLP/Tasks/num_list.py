73b0792fe860e471227c17ab3251f115793be7c7
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-18 07:28:46 +0300

    printing the values of a list

---
+++ Python_PLP/Tasks/num_list.py
@@ -0,0 +1,8 @@
+# Having a list and printing out the values
+print("...Priting out the values of a list...")
+# Declare the list
+num_list = [33,42,5,66,77,22,16,79,36,62,78,43,88,39,53,67,89,11]
+# Print out the values sequntially
+for values in num_list:
+    print(values)
+    
