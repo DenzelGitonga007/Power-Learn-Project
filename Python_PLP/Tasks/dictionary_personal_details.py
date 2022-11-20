ae5f598fc24127556e4e4cee741c20ace75134c5
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-20 19:36:54 +0300

    Updating the contents of the dictionary

--- Python_PLP/Tasks/dictionary_personal_details.py
+++ Python_PLP/Tasks/dictionary_personal_details.py
@@ -3,3 +3,9 @@
 print("...Personal details...")
 # Initialize the dictionary
 personalDetails = {"FirstName": "James", "LastName": "Ouko", "Age": 22, "PhoneNumber": "07123456789", "Email": "student@gmail.com"}
+print("The original dictionary is: ", personalDetails)
+# Update Age to 55
+print("...Updating age to 55...")
+new_age = {"Age": 50}
+personalDetails.update(new_age)
+print("With age updated: ", personalDetails)
