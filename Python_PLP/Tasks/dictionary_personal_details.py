52dcff1da5f0963c104865e357c0b3d66bea0898
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-20 19:24:02 +0300

    Declared the dictionary and working well

---
+++ Python_PLP/Tasks/dictionary_personal_details.py
@@ -0,0 +1,5 @@
+# Create a dictionary named personalDetails with the following information ( 5 marks)
+# FisrtName James, LastName Ouko, Age 22, PhoneNumber 07123456789, Email student@gmail.com
+print("...Personal details...")
+# Initialize the dictionary
+personalDetails = {"FirstName": "James", "LastName": "Ouko", "Age": 22, "PhoneNumber": "07123456789", "Email": "student@gmail.com"}
