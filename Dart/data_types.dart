e579b6369ea2351e0321ff32304d8e4ecfc51e71
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-22 11:57:29 +0300

    Datatypes

---
+++ Dart/data_types.dart
@@ -0,0 +1,11 @@
+// Trying out datatypes in dart
+// 1. Numbers
+void main() {
+int num = 7;
+double num_2 = 5;
+// Getting the variable type
+// ....
+print("$num is of type interger");
+print("$num_2 is a double");
+
+}
