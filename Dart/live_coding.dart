3998c0f3400008490dac33dbb0535098f2a28efa
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-22 19:56:49 +0300

    Live coding, Dart

---
+++ Dart/live_coding.dart
@@ -0,0 +1,29 @@
+void main() {
+  // 1. Write a dart program that outputs your name,nationality and your hobby.
+  String name = "Denzel";
+  String nationality = "Kenyan";
+  String hobby = "memes";
+  // Printing them individually
+  // print(name + nationality + hobby);
+  print(name);
+  print(nationality);
+  print(hobby);
+  // 2. Put the name,nationality and hobby above in variables and output in one line.
+  print("I am $name, from $nationality, and I like watching $hobby");
+  // 3. Perform string interpolation.
+  String text = "Powerlearnproject";
+  print("The name '$text' has ${text.length} characters");
+  // 4. Show the index of a specific letter in a string
+  String index_test = "Murife";
+  // var index = index_test.indexOf("e");
+  print("The letter 'e' is at position ${index_test.indexOf("e")}");
+  // 5. Create a list of 5 capital cities in Africa
+  List capital_cities = ["Nairobi", "Accra", "Lagos", "Cape Town", "Kampala", "Dar-el-salaam"];
+  print("The capital cities are: $capital_cities");
+  // 6. Add two capital cities to the list created.
+  capital_cities.add("Kakuma");
+  capital_cities.add("Mogadishu");
+  print("After independence, the cities are: $capital_cities");
+
+
+}
