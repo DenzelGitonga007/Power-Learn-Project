2a24ec204855f33eb7768eaa53795c8b1267bb62
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-11-23 14:25:01 +0300

    string length

---
+++ Group_Assignment(s)/Python_Work/str_length.py
@@ -0,0 +1,8 @@
+# Write a Python program that prints the length of a string.
+# For example of expected results;
+# Input Output Explanation
+# "" 0 0 because its empty string
+# “Jambo” 5 5 because it has 5 characters
+print("...Finding the length of a string...")
+word = input("Enter the text to find length for: ")
+print("{} {} because is has {} characters".format(len(str(word)), len(str(word)), len(str(word))))
