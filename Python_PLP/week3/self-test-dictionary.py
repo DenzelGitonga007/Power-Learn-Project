cd9d1237bdc5916e23ca63e363d151e2541f9895
DenzelGitonga007 <gitongadenzel@gmail.com>
2022-12-05 10:58:06 +0300

    personal details dictionary

---
+++ Python_PLP/week3/self-test-dictionary.py
@@ -0,0 +1,23 @@
+# Create a dictionary named personalDetails with the following information ( 5 marks)
+# FisrtName James
+# LastName Ouko
+# Age 22
+# PhoneNumber 07123456789
+# Email student@gmail.com
+# Using the relevant properties and methods do the following;
+# Update Age to 55                                       
+# Print the LastName                                      
+# Print length of the dictionary
+
+# Start
+print("...Personal Details...")
+# Create the dictionary
+personalDetails = {
+    "FirstName" : "James",
+    "LastName" : "Ouko",
+    "Age" : 22,
+    "PhoneNumber" : "07123456789",
+    "Email" : "student@gmail.com"
+}
+# Show the initial dictionary
+print("Your details at present are:",personalDetails)
