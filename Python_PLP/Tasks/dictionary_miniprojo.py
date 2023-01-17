569598f86d21d555b73d83258a95a23ec262c850
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-17 15:06:45 +0300

    dictionary app in python

---
+++ Python_PLP/Tasks/dictionary_miniprojo.py
@@ -0,0 +1,10 @@
+# In this project, you are tasked to create a program that automates searching for a word definition in a dictionary. Follow the instructions to implement
+# Dictionary Project is provided here:
+# We should have a data source (Download from the link above)
+# Learn how to load json data into a python dictionary
+# Create a function that returns a definition of a word
+# Consider a condition that the entered word is not in a dictionary
+# Consider input from user having different cases – upper/ lower case or mixed eg: RAIN/rain/RaIN
+# Make your dictionary program more intelligent incase users input a word with wrong spelling 
+# the program should be able to suggest the word that might be intended.
+# eg . pott instead of pot or rainn instead of rain. Tip: use difflib library here
