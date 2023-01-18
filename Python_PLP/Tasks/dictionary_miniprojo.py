3d8c61cb4d8113b8237cb2b65d0296936bc7624f
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-18 10:29:31 +0300

    importing the json file

--- Python_PLP/Tasks/dictionary_miniprojo.py
+++ Python_PLP/Tasks/dictionary_miniprojo.py
@@ -1,4 +1,5 @@
-# In this project, you are tasked to create a program that automates searching for a word definition in a dictionary. Follow the instructions to implement
+# In this project, you are tasked to create a program that automates searching for a word definition in a dictionary. 
+# Follow the instructions to implement
 # Dictionary Project is provided here:
 # We should have a data source (Download from the link above)
 # Learn how to load json data into a python dictionary
@@ -8,3 +9,7 @@
 # Make your dictionary program more intelligent incase users input a word with wrong spelling 
 # the program should be able to suggest the word that might be intended.
 # eg . pott instead of pot or rainn instead of rain. Tip: use difflib library here
+
+
+
+
