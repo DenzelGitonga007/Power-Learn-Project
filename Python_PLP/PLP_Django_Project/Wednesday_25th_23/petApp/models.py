eab815656238d31a6569096e4fb50c196ea33cc7
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-25 20:07:11 +0300

    making the models and migrations

--- Python_PLP/PLP_Django_Project/Wednesday_25th_23/petApp/models.py
+++ Python_PLP/PLP_Django_Project/Wednesday_25th_23/petApp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+# Two tables: pets and vaccines-- Many to many
+
+# Create a model for vaccines model
+class Vaccines(models.Model):
+    # The attributes/ fields
+    name = models.CharField(max_length=155, unique=True)
+
+# Pet model/table
+class Pet(models.Model):
+
+    GENDER_CHOICES = [("M", "Male"),
+    ("F", "Female")
+    ] # For gender
+    name = models.CharField(max_length=155)
+    owner = models.CharField(max_length=155)
+    submitted_on = models.DateTimeField()
+    species = models.CharField(max_length=100)    
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+    description = models.TextField(blank=True, null=True)
+
+    # Relationship
+    vaccines = models.ManyToManyField(Vaccines)
