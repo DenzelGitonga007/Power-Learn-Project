7cb0acdea339d737f197fc8bdbbee92d31aa052e
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-29 21:56:32 +0300

    template

--- Python_PLP/PLP_Django_Project/sunday_29th_jan_django_tution/formsProject/settings.py
+++ Python_PLP/PLP_Django_Project/sunday_29th_jan_django_tution/formsProject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'formsProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
