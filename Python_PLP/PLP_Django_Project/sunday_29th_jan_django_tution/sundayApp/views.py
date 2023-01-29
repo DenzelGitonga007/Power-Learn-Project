1cab1bd5a4ab8b3612f9480f8973ffde430f46bf
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-01-29 21:10:22 +0300

    views for the forms

--- Python_PLP/PLP_Django_Project/sunday_29th_jan_django_tution/sundayApp/views.py
+++ Python_PLP/PLP_Django_Project/sunday_29th_jan_django_tution/sundayApp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+# Import the forms
+from .forms import ApplicationForm
 
 # Create your views here.
+
+def sunday_view(request):
+    form = ApplicationForm()
+    return render(request, 'application.html', {})
