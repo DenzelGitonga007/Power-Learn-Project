0ea08fc9db123f574664f301a840077eb951f292
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-01 19:54:53 +0300

    model

--- Python_PLP/PLP_Django_Project/plp2/plp2_App/models.py
+++ Python_PLP/PLP_Django_Project/plp2/plp2_App/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Application(models.Model):
+
+    GENDER_CHOICES = (
+        ('F', 'Female'),
+        ('M', 'Male')
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    gender = models.CharField(max_length=2, choices=GENDER_CHOICES)
+
+    # to return the person's details
+    def __str__(self):
+        return self.email
+
+    class Meta: # meta holds info about the model
+        db_table = "Applications" # the table will be called applications
