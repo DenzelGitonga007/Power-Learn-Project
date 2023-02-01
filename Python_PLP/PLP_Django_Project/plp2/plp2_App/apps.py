63af522c98633a06acd4fe90940a945243926f85
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-01 19:29:24 +0300

    plp2_App

---
+++ Python_PLP/PLP_Django_Project/plp2/plp2_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Plp2AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'plp2_App'
--- Python_PLP/PLP_Django_Project/plp2/plp2_project/settings.py
+++ Python_PLP/PLP_Django_Project/plp2/plp2_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'plp2_App',
 ]
 
 MIDDLEWARE = [
