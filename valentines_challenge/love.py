0255795f25db42fce41772e71f42298a6f306045
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-13 20:27:02 +0300

    welcome note

--- valentines_challenge/love.py
+++ valentines_challenge/love.py
@@ -3,7 +3,9 @@
 # Import time and sleep for slow typing
 import sys, time
 # from time import sleep
-welcome_note = "💖💖💖...Welcome to Denzel's Valentines Heart Warmer 🤗🥰💖...\n...Get to celebrate your valentines in style..."
+welcome_note = "💖💖💖...Welcome to Denzel's Valentines Heart Warmer 🤗🥰💖...\n...Get to celebrate your valentines in style 😲🎉🥳...\n"
 for letter in welcome_note:
-    time.sleep(.1)
-    print(letter.encode("utf-8"), end='')
+    time.sleep(.1) # to print slowly
+    print(letter, end='')
+
+
