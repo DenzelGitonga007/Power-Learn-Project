20f4250b592e98c8565da899ad33cfd4dbe69ee5
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-15 20:09:56 +0300

    favicon

--- Python_PLP/PLP_Django_Project/recap/denzelProject/settings.py
+++ Python_PLP/PLP_Django_Project/recap/denzelProject/settings.py
@@ -118,6 +118,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    'denzelApp\static'
+] # include the static fdl
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
