80371892d6feba3db62d1829e5f46f90bb2d42df
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-20 20:01:57 +0300

    retrieve details

--- bootcamp/housesApp/urls.py
+++ bootcamp/housesApp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.houseListings, name = "home"),
+    path('', views.houseListings, name = "home"), # list all houses
+    path('details/', views.retrieveDetails, name = "details")
 ]
--- bootcamp/housesApp/views.py
+++ bootcamp/housesApp/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from .models import houseListingModel
 # Create your views here.
 """
-CRUD - Create, Read/Retrieve, Update, Delete 
+CRUD - Create, Read/Retrieve, Update, Delete, listing all elements
 """
+
+# List all the houses
 def houseListings(request):
     # select * from houseListingsModel/table
     listings = houseListingModel.objects.all()
     context = {
-        "listings" : listings
+        "listings" : listings,
     }
     return render(request, 'listings.html', context)
+
+# CRUD - Read/Retrieve
+# View each house by itself
+def retrieveDetails(request, pk):
+    # Get the house by id
+    specific_house = houseListingModel.objects.get(id = pk)
+    context = {
+        "specific" : specific_house
+    }
+    return render(request, 'details.html', context) 
