288c1d2ceb1c3f9850397908d3ef73c476f4e7a5
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-20 18:41:06 +0300

    migrate and create superuser

--- bootcamp/housesApp/models.py
+++ bootcamp/housesApp/models.py
@@ -9,7 +9,7 @@ class houseListingModel(models.Model):
     square_feet = models.IntegerField()
     no_of_bedrooms = models.IntegerField()
     address = models.CharField(max_length=255)
-    image = models.ImageField()
+    image = models.ImageField(upload_to = 'uploads/')
 
     # def __str__(self):
     #     return self.title
