0968decd8872d07484da4ccf5b166bbf447bfc86
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-20 18:20:50 +0300

    registered the models

--- bootcamp/housesApp/admin.py
+++ bootcamp/housesApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+# from the houseApp folder, and in the models file, import the class houseListingModel
+from .models import houseListingModel 
 
 # Register your models here.
+admin.site.register(houseListingModel)
