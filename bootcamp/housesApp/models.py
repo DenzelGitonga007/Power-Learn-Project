80217f1777bb7148392a78ca75b081f0c1f59315
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-20 18:08:03 +0300

    models, house listing model

--- bootcamp/housesApp/models.py
+++ bootcamp/housesApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class houseListingModel(models.Model):
+    # the attributes/fields/columns
+    title = models.CharField(max_length=255)
+    price = models.IntegerField()
+    square_feet = models.IntegerField()
+    no_of_bedrooms = models.IntegerField()
+    address = models.CharField(max_length=255)
+    image = models.ImageField()
+    
