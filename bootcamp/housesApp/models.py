d300dca5c36da1c6bf154b67685f8629cf56ab35
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-20 19:13:51 +0300

    CRUD-- list all

--- bootcamp/housesApp/models.py
+++ bootcamp/housesApp/models.py
@@ -11,5 +11,6 @@ class houseListingModel(models.Model):
     address = models.CharField(max_length=255)
     image = models.ImageField(upload_to = 'uploads/')
 
-    # def __str__(self):
-    #     return self.title
+    # present the strings/columns on the UI
+    def __str__(self):
+        return self.title
--- bootcamp/housesApp/views.py
+++ bootcamp/housesApp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import houseListingModel
 # Create your views here.
+"""
+CRUD - Create, Read/Retrieve, Update, Delete 
+"""
+def houseListings(request):
+    # select * from houseListingsModel/table
+    listings = houseListingModel.objects.all()
+    context = {
+        "listings" : listings
+    }
+    return render(request, 'listings.html', context)
