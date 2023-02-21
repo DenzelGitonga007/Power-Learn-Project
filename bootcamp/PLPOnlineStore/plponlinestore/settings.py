0c3b8e6bb5141ddf326e8f80016115b8da237f52
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-21 18:08:42 +0300

    urls

--- bootcamp/PLPOnlineStore/plponlinestore/settings.py
+++ bootcamp/PLPOnlineStore/plponlinestore/settings.py
@@ -121,3 +121,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATIC_URL = '/static/'
+
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/img/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/img')
