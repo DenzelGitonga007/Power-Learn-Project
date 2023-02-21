cfa0fc291cd503e74a4ed846975babcfd1fcdd28
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-21 18:34:02 +0300

    the home.html

--- bootcamp/PLPOnlineStore/home/views.py
+++ bootcamp/PLPOnlineStore/home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+# The home function
+def home(request):
+    return render(request, 'home/home.html', {}) # referencing the home.html in home folder in templates
