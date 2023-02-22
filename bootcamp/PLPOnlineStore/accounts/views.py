83ca570bea827f024082bc67c6bf7d11c27859c1
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-22 20:50:39 +0300

    refactor the sign up form

--- bootcamp/PLPOnlineStore/accounts/views.py
+++ bootcamp/PLPOnlineStore/accounts/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group, User
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, logout
 from .forms import CustomerSignUpForm
 # Create your views here.
+# login
 def login(request):
-    pass
-
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST) # already existing data
+        if form.is_valid():
+            # Fields to authenticate/confirm
+            username = request.POST['username']
+            password = request.POST['password']
+            # Authenticate
+            user = authenticate(username=username, password=password)
+            if user is not None: # if user exits.authenticated
+                login(request, user)
+                return redirect('home')
+            else:
+                return redirect('register')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
+# register
 def register(request):
     if request.method == 'POST': # If user posts data
         form = CustomerSignUpForm(request.POST)
@@ -18,6 +34,10 @@ def register(request):
             customer_group = Group.objects.get(name='Customer')
             customer_group.user_set.add(signup_user)
     else:
-        form = CustomerSignUpForm(request.POST)
+        form = CustomerSignUpForm()
     return render(request, 'registration/register.html', {'form':form})
+
+# Logout
+def logout(request):
+    pass
 
