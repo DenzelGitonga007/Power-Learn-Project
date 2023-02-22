a471938a346a318a53651444c753a4403a9c8eb1
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-02-22 21:52:41 +0300

    models for the store-- product and categories

--- bootcamp/PLPOnlineStore/category/models.py
+++ bootcamp/PLPOnlineStore/category/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Category(models.Model):
+
+    STATUS = (
+        ('True', 'True')
+        ('False', 'False')
+    )
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    keyword = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'caregories'
+
+    def get_url(self):
+        return reverse('products_bycategory', args=[self.slug])
+
+    def __str__(self):
+        return self.name
--- bootcamp/PLPOnlineStore/store/models.py
+++ bootcamp/PLPOnlineStore/store/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+from category.models import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    description = models.TextField(null=True, blank=True, max_length=1000)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    stock = models.IntegerField()
+    available = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+    
+    def get_url(self):
+        return reverse('products_detail', args=[self.category.slug, self.slug])
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
