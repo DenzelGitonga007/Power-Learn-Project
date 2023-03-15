a4e3364ced5e65109e56d58968d788f3d837f5ac
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-03-15 21:07:32 +0300

    product detail

--- Python_PLP/PLP_Django_Project/auth_system/auth_project/settings.py
+++ Python_PLP/PLP_Django_Project/auth_system/auth_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'auth_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, "templates")],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- Python_PLP/PLP_Django_Project/auth_system/crudApp_two/views.py
+++ Python_PLP/PLP_Django_Project/auth_system/crudApp_two/views.py
@@ -4,10 +4,20 @@ from . models import Product
 
 # Create your views here.
 # CRUD -- Create, Read, Update, Delete
+# List the product
 def product_listing(request):
     products = Product.objects.all()
     # Display the products to the fron-end
     context = {
         "product": products
     }
-    return render(request, "listing_p.html", context)
+    return render(request, "listings_p.html", context)
+
+# Product by detail
+def product_details(request, pk):
+    # product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
+    context = {
+        "product": product,
+    }
+    return render(request, "product_details.html", context)
