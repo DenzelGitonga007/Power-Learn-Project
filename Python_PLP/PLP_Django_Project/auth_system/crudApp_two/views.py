f356bf05bc2ddc760d3625e20d1d4b926f2b87c4
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-03-15 20:36:16 +0300

    views

--- Python_PLP/PLP_Django_Project/auth_system/crudApp_two/views.py
+++ Python_PLP/PLP_Django_Project/auth_system/crudApp_two/views.py
@@ -1,2 +1,13 @@
 from django.shortcuts import render
+# Import the views
+from . models import Product
 
+# Create your views here.
+# CRUD -- Create, Read, Update, Delete
+def product_listing(request):
+    products = Product.objects.all()
+    # Display the products to the fron-end
+    context = {
+        "product": products
+    }
+    return render(request, "listing_p.html", context)
