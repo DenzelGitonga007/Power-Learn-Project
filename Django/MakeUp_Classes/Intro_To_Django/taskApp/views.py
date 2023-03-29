093221a850eb69cd428fed79f2a2b32f1c9d36c5
DenzelGitonga007 <gitongadenzel@gmail.com>
2023-03-29 21:05:56 +0300

    CRUD R-Read

--- Django/MakeUp_Classes/Intro_To_Django/taskApp/views.py
+++ Django/MakeUp_Classes/Intro_To_Django/taskApp/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+from . models import Task
 
 # Create your views here.
 def taskList(request):
-    return render(request, "tasks/tasklist.html")
+    tasks = Task.objects.all()
+    context = {
+        "tasks" : tasks
+    }
+    return render(request, "tasks/tasklist.html", context)
 
 # View task by ID
 def taskDetail(request, pk):
